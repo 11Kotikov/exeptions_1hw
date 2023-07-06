6fbaf9e7b9e259c8264accab056039efffb08281
Kotikov <11deadsluts@gmail.com>
2023-07-06 13:23:13 +0300

    all is done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,16 +4,44 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-//        exceptionReadTester(); //какие ещё ошибки могут возникнуть?
-//        /* ошибки могут быть связаны с разрешенным доступом,
+        // Реализуйте 3 метода, чтобы в каждом из них получить разные исключения,
+        //посмотрите на код, и подумайте сколько разных типов исключений вы тут сможете получить?
+//
+// метод 1
+        exceptionReadTester(); //какие ещё ошибки могут возникнуть?
+//        /* Ошибки могут быть связаны с разрешенным доступом,
 //         введёнными недопустимыми символами...
 //         */
-//        sumNum(); // я не знаю почему, но на 17й и 20й версии jdk выдаёт - "1" в консоль
+// метод 2
+        sumNum(); // я не знаю почему, но на 17й и 20й версии jdk выдаёт - "1" в консоль
 //        //вместо ArithmeticException (>.<)
+// метод 3
         canNotDivide();
+
+        //Реализуйте метод, принимающий в качестве аргументов два целочисленных массива,
+        // и возвращающий новый массив, каждый элемент которого равен частному элементов
+        // двух входящих массивов в той же ячейке.
+
+        List<Integer> arr1 = new ArrayList<>();
+        arr1.add(10);
+        arr1.add(20);
+        arr1.add(30);
+        arr1.add(40);
+        arr1.add(50);
+
+        List<Integer> arr2 = new ArrayList<>();
+        arr2.add(2);
+        arr2.add(4);
+        arr2.add(3);
+        arr2.add(8);
+
+        List<Double> resultArray = divideArrays(arr1, arr2);
+        System.out.println(resultArray);
     }
 
     public static void exceptionReadTester() {
@@ -28,10 +56,12 @@ public class Main {
 
     public static void sumNum() {
         try {
-            int a,b, prod;
-            a =  2_147_483_647;;
-            b =  2_147_483_647;;
-            prod = a*b;
+            int a, b, prod;
+            a = 2_147_483_647;
+            ;
+            b = 2_147_483_647;
+            ;
+            prod = a * b;
             System.out.println(prod);
         } catch (ArithmeticException e) {
             System.out.println("From -2 147 483 647 to 2 147 483 647" + e.getMessage());
@@ -40,7 +70,7 @@ public class Main {
 
     public static void canNotDivide() {
         try {
-            int result = 10 / 0;
+            int result = 0 / 0;
             System.out.println("result: " + result);
         } catch (ArithmeticException e) {
 
@@ -48,6 +78,23 @@ public class Main {
         } finally {
             System.out.println("all 3 methods are done");
         }
+    }
+
+    public static List<Double> divideArrays(List<Integer> array1, List<Integer> array2) {
+        List<Double> result = new ArrayList<>();
+        try {
+            for (int i = 0; i < array1.size(); i++) {
+                if (array2.get(i) != 0) {
+                    result.add((double) array1.get(i) / array2.get(i));
+                } else {
+                    throw new RuntimeException("can't be divided by 0");
+                }
+            }
+            return result;
+        } catch (Exception e) {
+            throw new RuntimeException("Index out of bounds, you can only divide arrays of the same size");
+        }
+
     }
 
 }
